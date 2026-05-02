a739b5614bded2d0109c16d63168dd937a8ceff9
tharagy <tharagythayananthan25@gmail.com>
2026-05-02 23:25:28 +0530

    add changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,10 +5,14 @@ function About() {
     <>
     <header className="about">
        <h1> About Directory</h1>
-       <p>Empowering student managementwith modern tools</p>
+       <p>Empowering student management with modern tools.</p>
        
        </header>
-       <div></div>
+       <div class="details">
+
+        <h2>Streamelined.Secure.Simple</h2>
+        <p>We created the Student Directory Appto bridge the gapbetween complex data and user-friendly interfaces.</p>
+       </div>
 </>
   )
 }
